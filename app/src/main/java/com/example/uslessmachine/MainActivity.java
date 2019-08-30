06ae4437e0654a346451e551dff3b6451dfa3580
zignatious <heysigmund@gmail.com>
2019-08-30 13:58:31 -0700

    Blink BLink self destruct.

--- app/src/main/java/com/example/uslessmachine/MainActivity.java
+++ app/src/main/java/com/example/uslessmachine/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.uslessmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -13,23 +22,25 @@ public class MainActivity extends AppCompatActivity {
     private Switch switchUsless;
     private Button buttonSelfDestruct;
     private Button buttonDoSomething;
-
+    private ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         wiredWidgets();
-        
+
         setListeners();
     }
+
 
     private void wiredWidgets() {
         switchUsless = findViewById(R.id.switch_main_usless);
         buttonSelfDestruct = findViewById(R.id.button_main_selfdestruct);
         buttonDoSomething = findViewById(R.id.button_main_busy);
-
+        constraintLayout = findViewById(R.id.constraint_layout_main);
     }
+
 
     private void setListeners() {
         switchUsless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -56,10 +67,61 @@ public class MainActivity extends AppCompatActivity {
 //                    Toast.makeText(MainActivity.this, "SWITCH GO ON", Toast.LENGTH_SHORT);
 //                }else {
 //                    Toast.makeText(MainActivity.this, "SWITCH GO OFF", Toast.LENGTH_SHORT);
-//                }
+//
+//
+//            }
+
+
+
+                //set the onclick listener for the self destruct button
+                //make a 10 second countdown timer
+                // display how much time is left on the countdown on the button
+                //when the timer is complete, call the finish() method to close
+                //the activity
+                //make the background blink red
+                //make the background blink red increasingly faster as the time runs out
+
                 }
 
         });
+                  buttonSelfDestruct.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View view) {
+                          new CountDownTimer(10000, 1000) {
+
+                              public void onTick(long millisUntilFinished) {
+                                  buttonSelfDestruct.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+                                  final ConstraintLayout layout =  findViewById(R.id.constraint_layout_main);
+                                  final AnimationDrawable drawable = new AnimationDrawable();
+                                  final Handler handler = new Handler();
+
+                                  drawable.addFrame(new ColorDrawable(Color.RED), 400);
+                                  drawable.addFrame(new ColorDrawable(Color.WHITE), 400);
+                                  drawable.setOneShot(false);
+
+                                  layout.setBackgroundDrawable(drawable);
+                                  handler.postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          drawable.start();
+                                      }
+                                  }, 100);
+                              }
+
+                              public void onFinish() {
+                                  buttonSelfDestruct.setText("BOOM!");
+
+                                  finish();
+
+                              }
+                          }.start();
+                      }
+                  });
+
+
+
+
     }
 
 }
